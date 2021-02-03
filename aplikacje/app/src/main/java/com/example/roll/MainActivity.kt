5e0df40a13d7a0a365dc7b1f3099104432c8b16e
Feliks Czart <czart78@gmail.com>
2021-02-03 21:03:31 +0100

    part 1

---
+++ aplikacje/app/src/main/java/com/example/roll/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.roll
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val rollButton = findViewById<Button>(R.id.rollButton)
+//        val resultsTextViev = findViewById<TextView>(R.id.howMuch)
+//        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+
+//        rollButton.setOnClickListener{
+//            val rand = Random().nextInt(seekBar.progress) + 1
+//            resultsTextViev.text = rand.toString()
+//        }
+    }
+}
